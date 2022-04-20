04f2a47d6040b233b1b52c30755a5933f5712229
Jorge Pissarra <jorgepissarra@protonmail.com>
2022-04-20 11:15:54 +0100

    Removed redundant file and added a connection file

--- Servidor/src/Servidor.java
+++ Servidor/src/Servidor.java
@@ -2,29 +2,38 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 
+
 public class Servidor {
 	public static void main(String [] args) {
-        // instalar um gestor de segurança
+		// instalar um gestor de seguran\ufffda
     	System.setSecurityManager(new SecurityManager());
-		try {
-			// inicializar a execução do registo no porto desejado
+    	
+    	try {
+			// inicializar a execuï¿½ï¿½o do registo no porto desejado
 			java.rmi.registry.LocateRegistry.createRegistry(1099);
     		System.out.println("Registo RMI pronto.");
 		} catch (RemoteException e) {
 			System.out.println(e.getMessage());
 		}
-    	try {
-    		// instanciar objeto remoto do Produtor
-    		InterfaceProdutor produtor = new ImplementacaoProdutor();
-    		InterfaceConsumidor consumidor = new ImplementacaoConsumidor();
-    		// registar o objeto remoto do produtor no Registry
-    		Naming.rebind("Produtor", produtor);
-    		System.out.println("Objeto remoto do Produtor pronto.");
-    		// registar o objeto remoto do cliente no Registry
-    		Naming.rebind("Consumidor", consumidor);
-    		System.out.println("Objeto remoto do Consumidor pronto.");
-    	} catch (MalformedURLException | RemoteException e) {
-    		System.out.println(e.getMessage());
-    	}
+		while(true) {
+			try {
+				
+				
+	    		// instanciar objeto remoto do Produtor
+	    		InterfaceProdutor produtor = new ImplementacaoProdutor();
+	    		InterfaceConsumidor consumidor = new ImplementacaoConsumidor();
+	    		
+	    		// registar o objeto remoto do produtor no Registry
+	    		Naming.rebind("Produtor", produtor);
+	    		System.out.println("Objeto remoto do Produtor pronto.");
+	    		
+	    		// registar o objeto remoto do cliente no Registry
+	    		Naming.rebind("Consumidor", consumidor);
+	    		System.out.println("Objeto remoto do Consumidor pronto.");
+	    		
+	    	} catch (MalformedURLException | RemoteException e) {
+	    		System.out.println(e.getMessage());
+	    	}
+		}
     }
 }
